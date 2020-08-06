6c11357fc8931c22edf0780e81f8ebdb6c868302
Eric Keenan <eric.keenan@colorado.edu>
2020-08-06 10:00:09 -0600

    Add main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+
+"""
+Git Tutorial
+
+To execute this script from the command line, execute: 
+python3 main.py
+
+"""
+
+# Group 1
+# print("Group 1")
+
+# Group 2
+# print("Group 2")
+
+# Group 3
+# print("Group 3")
+
+# Group 4
+# print("Group 4")
+
+
+# Group 5
+# print("Group 5")
+
+
+# Group 6
+# print("Group 6")
+
+
