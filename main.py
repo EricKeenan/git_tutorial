15a71cc68d284d0ac4dd4b38604a631fb626b8b1
EricKeenan <eric.keenan@colorado.edu>
2020-09-29 09:11:59 -0600

    Add more groups

--- main.py
+++ main.py
@@ -19,9 +19,32 @@ print("Group 1")
 # Group 4
 # print("Group 4")
 
-
 # Group 5
 # print("Group 5")
 
 # Group 6
 # print("Group 6")
+
+# Group 7
+# print("Group 7")
+
+# Group 8
+# print("Group 8")
+
+# Group 9
+# print("Group 9")
+
+# Group 10
+# print("Group 10")
+
+# Group 11
+# print("Group 11")
+
+# Group 12
+# print("Group 12")
+
+# Group 13
+# print("Group 13")
+
+# Group 14
+# print("Group 14")
